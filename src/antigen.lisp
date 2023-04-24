340c0d2ef8fda7e20111a591af61d72289fa31e5
Dennis Dunn <ansofive@gmail.com>
2023-04-24 19:59:16 -0400

    Simplify until there is no reason to change.

--- src/antigen.lisp
+++ src/antigen.lisp
@@ -1,25 +1,24 @@
 (in-package :cl-user)
 
 (defpackage :cdsi-data/antigen
-  (:nicknames :antigen)
   (:use :cl
         :cdsi-data/config)
-  (:export :get-antigen
-           :catalog))
+  (:export :list-antigens
+           :get-antigen))
 
 (in-package :cdsi-data/antigen)
 
-(defun get-antigen (id)
-       "Load and parse the antigen file identified by the name."
-       (let* ((fname (ppcre:regex-replace "[(.*)]+" *antigen-file-re* id))
-              (path (merge-pathnames *data-path* fname)))
-             (xmls:parse (uiop:read-file-string path))))
+(defun list-antigens ()
+  "Return a list of antigen names."
+  (remove-if #'null
+      (mapcar (lambda (fname)
+                (ppcre:register-groups-bind (antigen)
+                                            (*antigen-file-re* (namestring fname))
+                                            antigen))
+          (uiop:directory-files *data-path*))))
 
-(defun catalog ()
-       "Return a list of antigen names."
-       (remove-if #'null
-                  (mapcar (lambda (fname)
-                                  (ppcre:register-groups-bind (antigen)
-                                    (*antigen-file-re* (namestring fname))
-                                    antigen))
-                          (uiop:directory-files *data-path*))))
+(defun get-antigen (id)
+  "Load and parse the antigen file identified by the name."
+  (let* ((fname (ppcre:regex-replace "[(.*)]+" *antigen-file-re* id))
+         (path (merge-pathnames *data-path* fname)))
+    (xmls:parse (uiop:read-file-string path))))
--- src/schedule.lisp
+++ src/schedule.lisp
@@ -1,14 +1,21 @@
 (in-package :cl-user)
 
 (defpackage :cdsi-data/schedule
-  (:nicknames :schedule)
   (:use :cl
         :cdsi-data/config)
-  (:export :get-schedule))
+  (:export :list-schedules
+           :get-schedule))
 
 (in-package :cdsi-data/schedule)
 
+(defun list-schedules ()
+  (mapcar #'xmls:node-name (xmls:xmlrep-children (load-schedule))))
 
-(defun get-schedule ()
-  "Get the schedule supporting data."
+(defun get-schedule (id)
+  (xmls:xmlrep-children (find-if #'(lambda (node)
+                                     (string= id (xmls:node-name node)))
+                          (xmls:xmlrep-children (load-schedule)))))
+
+(defun load-schedule ()
+  "Load the schedule supporting data."
   (xmls:parse (uiop:read-file-string (merge-pathnames *data-path* *schedule-file-name*))))
