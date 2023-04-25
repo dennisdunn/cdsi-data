a7dba62c5cd18f3058a9d0556c7c177736ddebc6
Dennis Dunn <ansofive@gmail.com>
2023-04-24 20:08:33 -0400

    Fixed the tests.

--- tests/main.lisp
+++ tests/main.lisp
@@ -7,13 +7,15 @@
 (in-package :cdsi-data/tests)
 
 (deftest test-the-antigen-data-access
-         (testing "fetch the antigen ids"
-                  (ok (= (length (antigen:catalog)) 25)))
-         (testing "load a apecific antigen supporting data"
-                  (let ((data (antigen:get-antigen "Diphtheria")))
-                    (ok (not (null data))))))
+  (testing "fetch the antigen ids"
+    (ok (= (length (cdsi-data/antigen:list-antigens)) 25)))
+  (testing "load a apecific antigen supporting data"
+    (let ((data (cdsi-data/antigen:get-antigen "Diphtheria")))
+      (ok (not (null data))))))
 
 (deftest test-the-schedule-data-access
-         (testing "load schedule supporting data"
-                  (let ((data (schedule:get-schedule)))
-                    (ok (not (null data))))))
+  (testing "fetch the schedule ids"
+    (ok (= (length (cdsi-data/schedule:list-schedules)) 5)))
+  (testing "load a apecific antigen supporting data"
+    (let ((data (cdsi-data/schedule:get-schedule "cvxToAntigenMap")))
+      (ok (not (null data))))))
